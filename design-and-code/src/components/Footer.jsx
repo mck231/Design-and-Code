ff4d9a310209beb6c7f830ec75be79871c0f99bd
Marco Cabrera <highlandsmac@yahoo.com>
2025-04-14 08:42:17 -0500

    restrucure app better

--- design-and-code/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-    return (
-        <div>
-            Design and code footer!
-        </div>
-    )
-}
-export default Footer
--- design-and-code/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return ( 
-        <div>
-            Design and Code Header
-        </div>
-    )
-}
-
-export default Header
